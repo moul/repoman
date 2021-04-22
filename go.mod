9b2afda30983a90b136ab846a5ec4547bada6af5
Manfred Touron <94029+moul@users.noreply.github.com>
2021-04-22 19:32:10 +0000

    feat: initial go version

--- go.mod
+++ go.mod
@@ -3,10 +3,12 @@ module moul.io/repoman
 go 1.13
 
 require (
+	github.com/peterbourgon/ff/v3 v3.0.0
 	github.com/tailscale/depaware v0.0.0-20201214215404-77d1e9757027
 	go.uber.org/goleak v1.1.10
+	go.uber.org/zap v1.16.0
 	moul.io/motd v1.0.0
 	moul.io/srand v1.6.1
-	moul.io/u v1.20.0
+	moul.io/u v1.20.0 // indirect
 	moul.io/zapconfig v1.3.0
 )
--- main.go
+++ main.go
@@ -1,31 +1,73 @@
 package main
 
 import (
+	"context"
+	"flag"
 	"fmt"
 	"math/rand"
 	"os"
 
+	"github.com/peterbourgon/ff/v3/ffcli"
+	"go.uber.org/zap"
 	"moul.io/motd"
 	"moul.io/srand"
-	"moul.io/u"
 	"moul.io/zapconfig"
 )
 
 func main() {
-	if err := run(os.Args); err != nil {
+	if err := run(os.Args[1:]); err != nil {
 		fmt.Fprintf(os.Stderr, "error: %v\n", err)
 		os.Exit(1)
 	}
 }
 
+var (
+	rootFs = flag.NewFlagSet("<root>", flag.ExitOnError)
+
+	logger *zap.Logger
+)
+
 func run(args []string) error {
 	rand.Seed(srand.Fast())
-	fmt.Print(motd.Default())
-	logger, err := zapconfig.Configurator{}.Build()
-	if err != nil {
+
+	// init logger
+	{
+		var err error
+		logger, err = zapconfig.Configurator{}.Build()
+		if err != nil {
+			return err
+		}
+	}
+
+	root := &ffcli.Command{
+		FlagSet: rootFs,
+		Subcommands: []*ffcli.Command{
+			{Name: "doctor", Exec: doDoctor},
+			{Name: "maintenance", Exec: doMaintenance},
+			{Name: "version", Exec: doVersion},
+		},
+	}
+
+	if err := root.ParseAndRun(context.Background(), args); err != nil {
 		return err
 	}
-	logger.Info("Hello World!")
-	fmt.Println("args", u.JSON(args))
+
+	return nil
+}
+
+func doDoctor(ctx context.Context, args []string) error {
+	logger.Info("DOCTOR")
+	return nil
+}
+
+func doMaintenance(ctx context.Context, args []string) error {
+	logger.Info("MAINTENANCE")
+
+	return nil
+}
+
+func doVersion(ctx context.Context, args []string) error {
+	fmt.Print(motd.Default())
+	fmt.Println("version: n/a")
 	return nil
 }
