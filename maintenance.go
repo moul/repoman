9b630c273a3e0bfc7a9a953048cad5fb22958c13
Manfred Touron <94029+moul@users.noreply.github.com>
2021-04-25 16:06:23 +0000

    feat: add support of bump-deps; and add various options to control the maintenance flow

--- maintenance.go
+++ maintenance.go
@@ -65,7 +65,7 @@ func doMaintenanceOnce(_ context.Context, path string) error {
 		}
 	}
 
-	if !project.Git.InMainBranch {
+	if opts.Maintenance.CheckoutMainBranch && !project.Git.InMainBranch {
 		logger.Debug("project is not using the main branch",
 			zap.String("current", project.Git.CurrentBranch),
 			zap.String("main", project.Git.MainBranch),
@@ -104,6 +104,8 @@ func doMaintenanceOnce(_ context.Context, path string) error {
 		}
 	}
 
+	initMoulBotEnv()
+
 	// TODO
 	// - repoman.yml ->
 	//   - template -> moul/golang-repo-template
@@ -112,73 +114,123 @@ func doMaintenanceOnce(_ context.Context, path string) error {
 	// - auto update from template
 	// - open PR / update existing one
 
-	script := `
+	if opts.Maintenance.BumpDeps {
+		logger.Debug("bumping deps", zap.String("project", project.Path))
+		// TODO: for each dirs with a go.mod, except vendor; overridable by repoman.yml
+		// TODO: overridable go binary
+		cmd := exec.Command("go", "get", "-u", "./...")
+		cmd.Stdout = os.Stderr
+		cmd.Stderr = os.Stderr
+		cmd.Dir = project.Path
+		cmd.Env = os.Environ()
+		if err := cmd.Run(); err != nil {
+			return fmt.Errorf("exec failed: %w", err)
+		}
+	}
+
+	if opts.Maintenance.Standard {
+		logger.Debug("applying standard changes", zap.String("project", project.Path))
+		script := `
+			main() {
+				# renovate.json
+				mkdir -p .github
+				git mv renovate.json .github/renovate.json || true
+				git rm -f renovate.json || true
+				cp ~/go/src/moul.io/golang-repo-template/.github/renovate.json .github/ || true
+				git add .github/renovate.json || true
+				git add renovate.json || true
+
+				# dependabot
+				cp ~/go/src/moul.io/golang-repo-template/.github/dependabot.yml .github/ || true
+				git add .github/dependabot.yml || true
+
+				# rules.mk
+				if [ -f rules.mk ]; then cp ~/go/src/moul.io/rules.mk/rules.mk .; fi || true
+
+				# authors
+				if [ -f rules.mk ]; then make generate.authors; git add AUTHORS; fi || true
+
+				# copyright
+				set -xe; \
+				for prefix in "©" "Copyright" "Copyright (c)"; do \
+					for file in README.md LICENSE-APACHE LICENSE-MIT LICENSE COPYRIGHT; do \
+						if [ -f "$file" ]; then \
+							sed -i "s/$prefix 2014 /$prefix 2014-2021 /" $file; \
+							sed -i "s/$prefix 2015 /$prefix 2015-2021 /" $file; \
+							sed -i "s/$prefix 2016 /$prefix 2016-2021 /" $file; \
+							sed -i "s/$prefix 2017 /$prefix 2017-2021 /" $file; \
+							sed -i "s/$prefix 2018 /$prefix 2018-2021 /" $file; \
+							sed -i "s/$prefix 2019 /$prefix 2019-2021 /" $file; \
+							sed -i "s/$prefix 2020 /$prefix 2020-2021 /" $file; \
+							sed -i "s/$prefix \([0-9][0-9][0-9][0-9]\)-20[0-9][0-9] /$prefix \1-2021 /" $file; \
+							sed -i "s/$prefix 2021-2021/$prefix 2021 /" $file; \
+						fi; \
+					done; \
+				done
+
+				# golangci-lint fix
+				sed -i "s/version: v1.26/version: v1.38/" .github/workflows/*.yml || true
+				sed -i "s/version: v1.27/version: v1.38/" .github/workflows/*.yml || true
+				sed -i "s/version: v1.28/version: v1.38/" .github/workflows/*.yml || true
+		       }
+		       main
+		`
+		cmd := exec.Command("/bin/sh", "-xec", script)
+		cmd.Stdout = os.Stderr
+		cmd.Stderr = os.Stderr
+		cmd.Dir = project.Path
+		cmd.Env = os.Environ()
+
+		err := cmd.Run()
+		if err != nil {
+			return fmt.Errorf("standard script execution failed: %w", err)
+		}
+	}
+
+	if opts.Maintenance.ShowDiff {
+		script := `
 		main() {
-			# renovate.json
-			mkdir -p .github
-			git mv renovate.json .github/renovate.json || true
-			git rm -f renovate.json || true
-			cp ~/go/src/moul.io/golang-repo-template/.github/renovate.json .github/ || true
-			git add .github/renovate.json || true
-			git add renovate.json || true
-
-			# dependabot
-			cp ~/go/src/moul.io/golang-repo-template/.github/dependabot.yml .github/ || true
-			git add .github/dependabot.yml || true
-
-			# rules.mk
-			if [ -f rules.mk ]; then cp ~/go/src/moul.io/rules.mk/rules.mk .; fi || true
-
-			# authors
-			if [ -f rules.mk ]; then make generate.authors; git add AUTHORS; fi || true
-
-			# copyright
-			set -xe; \
-			for prefix in "©" "Copyright" "Copyright (c)"; do \
-				for file in README.md LICENSE-APACHE LICENSE-MIT LICENSE COPYRIGHT; do \
-					if [ -f "$file" ]; then \
-						sed -i "s/$prefix 2014 /$prefix 2014-2021 /" $file; \
-						sed -i "s/$prefix 2015 /$prefix 2015-2021 /" $file; \
-						sed -i "s/$prefix 2016 /$prefix 2016-2021 /" $file; \
-						sed -i "s/$prefix 2017 /$prefix 2017-2021 /" $file; \
-						sed -i "s/$prefix 2018 /$prefix 2018-2021 /" $file; \
-						sed -i "s/$prefix 2019 /$prefix 2019-2021 /" $file; \
-						sed -i "s/$prefix 2020 /$prefix 2020-2021 /" $file; \
-						sed -i "s/$prefix \([0-9][0-9][0-9][0-9]\)-20[0-9][0-9] /$prefix \1-2021 /" $file; \
-						sed -i "s/$prefix 2021-2021/$prefix 2021 /" $file; \
-					fi; \
-				done; \
-			done
-
-			# golangci-lint fix
-			sed -i "s/version: v1.26/version: v1.38/" .github/workflows/*.yml || true
-			sed -i "s/version: v1.27/version: v1.38/" .github/workflows/*.yml || true
-			sed -i "s/version: v1.28/version: v1.38/" .github/workflows/*.yml || true
-
 			# apply changes
 			git diff
 			git diff --cached
+			git status
+		}
+		main
+	`
+		cmd := exec.Command("/bin/sh", "-xec", script)
+		cmd.Stdout = os.Stderr
+		cmd.Stderr = os.Stderr
+		cmd.Dir = project.Path
+		cmd.Env = os.Environ()
+
+		err := cmd.Run()
+		if err != nil {
+			return fmt.Errorf("publish script execution failed: %w", err)
+		}
+	}
+
+	// open a PR for the changes
+	{
+		script := `
+		main() {
+			# apply changes
 			git branch -D dev/moul/maintenance || true
 			git checkout -b dev/moul/maintenance
-			git status
 			git commit -s -a -m "chore: repo maintenance 🤖" -m "more details: https://github.com/moul/repoman"
 			git push -u origin dev/moul/maintenance -f
 			hub pull-request -m "chore: repo maintenance 🤖" -m "more details: https://github.com/moul/repoman" || hub pr list -f "- %pC%>(8)%i%Creset %U - %t% l%n"
 		}
 		main
 	`
-	// exec
-	{
 		cmd := exec.Command("/bin/sh", "-xec", script)
 		cmd.Stdout = os.Stderr
 		cmd.Stderr = os.Stderr
 		cmd.Dir = project.Path
-		initMoulBotEnv()
 		cmd.Env = os.Environ()
 
 		err := cmd.Run()
 		if err != nil {
-			return fmt.Errorf("script execution failed: %w", err)
+			return fmt.Errorf("publish script execution failed: %w", err)
 		}
 	}
 
